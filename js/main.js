d4d2c9c48fde08be3150420c2015d2c084e2dc57
Katia Savina <santaminus@gmail.com>
2016-07-18 09:35:41 +0300

    Corrected jQuery script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 use strict;
-function inputHandler() {
-    alert("Hello!")
-}
+$( document ).ready(function() {
+    $("#email").keypress(function(event) {
+        if(event.which == 13) 
+            alert("You have successfully subscribed to our news!");
+    });
+});
